216f9dbe34722094b32333921eb77f25f63f78ec
ATS <ATS@192.168.2.7>
2018-11-04 12:04:48 +0530

    /Akshay

--- src/main/java/com/ats/tril/repository/report/IndentReportRepo.java
+++ src/main/java/com/ats/tril/repository/report/IndentReportRepo.java
@@ -10,8 +10,24 @@ import com.ats.tril.model.report.IndentReport;
 
 public interface IndentReportRepo extends JpaRepository<IndentReport, Integer> {
 
-	@Query(value = "SELECT h.*,c.cat_desc,a.acc_head_desc,p.dept_desc,s.sub_dept_desc,t.type_name  FROM indent h,m_category c,m_account_head a,m_dept p ,m_sub_dept s,type t WHERE h.ind_m_id IN(:indentIdList) AND h.del_status=1 AND h.dept_id=p.dept_id AND h.sub_dept_id=s.sub_dept_id AND h.cat_id=c.cat_id AND a.acc_head_id=h.achd_id \r\n"
-			+ " and t.type_id=h.ind_m_type", nativeQuery = true)
+	@Query(value = "SELECT\r\n" + 
+			"        h.*,\r\n" + 
+			"        c.cat_desc,\r\n" + 
+			"        a.acc_head_desc,\r\n" + 
+			"        0 as dept_desc,\r\n" + 
+			"        0 as sub_dept_desc,\r\n" + 
+			"        t.type_name  \r\n" + 
+			"    FROM\r\n" + 
+			"        indent h,\r\n" + 
+			"        m_category c,\r\n" + 
+			"        m_account_head a, \r\n" + 
+			"        type t \r\n" + 
+			"    WHERE\r\n" + 
+			"        h.ind_m_id IN(:indentIdList) \r\n" + 
+			"        AND h.del_status=1  \r\n" + 
+			"        AND h.cat_id=c.cat_id \r\n" + 
+			"        AND a.acc_head_id=h.achd_id    \r\n" + 
+			"        and t.type_id=h.ind_m_type", nativeQuery = true)
 	List<IndentReport> getIndentHeaderReportList(@Param("indentIdList") List<Integer> indentIdList);
 
 	//
